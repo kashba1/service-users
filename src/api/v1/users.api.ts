749bdedecdf80d0ee349a0118f3344b462779863
AbhishekDhakad <abhishek.dhakad121@gmail.com>
2023-11-14 21:40:37 +0530

    completed verifyotp api

--- src/api/v1/users.api.ts
+++ src/api/v1/users.api.ts
@@ -1,11 +1,12 @@
 import { Express } from "express";
 import { authJwt, valHan, epoch, } from "../../middleware";
-import { sendOtp } from "../../controllers/v1/users.controller";
+import { sendOtp, verifyOtp } from "../../controllers/v1/users.controller";
 // import { SYNC_CONFIG } from "../../validationConfig/config";
 
 const router = (app: Express, options: any, urlSegment: string) => {
     // app.post(`${urlSegment}/sync`, [epoch.epochCheck, authJwt.deviceAuth, vendorCheck.checkVendorUsers, epoch.timeZoneCheck, valHan.validationHandler(SYNC_CONFIG)], multiSync);
-    app.post(`${urlSegment}/send-otp`, sendOtp)
+    app.post(`${urlSegment}/send-otp`, sendOtp),
+    app.post(`${urlSegment}/verify-otp`, verifyOtp)
 
 };
 
--- src/controllers/v1/users.controller.ts
+++ src/controllers/v1/users.controller.ts
@@ -7,19 +7,15 @@ import Api400Error from "../../utils/errorBase/api400Error";
 import redisUtils from "../../utils/redisKeys";
 import { getCache, setCache } from "../../repository/redis.repository";
 import { getUsers } from "../../repository/users.repository";
+import Api401Error from "../../utils/errorBase/api401Error";
 
 const generateRandomOtp = () => {
-    return Math.floor(1000 + Math.random() * 9000).toString();
+    return Math.floor(1000 + Math.random() * 9000);
 };
 
 export const sendOtp = async (req: any, res: Response, next: NextFunction) => {
     try {
         const { mobileNumber } = req.body;
-
-        if (!mobileNumber) {
-            // custom error mobile number required
-            throw new Api400Error()
-        }
 
         // const users = await getUsers({
         //     where: {
@@ -28,29 +24,28 @@ export const sendOtp = async (req: any, res: Response, next: NextFunction) => {
         // });
         // console.log(users);
 
-        // if (users) {
-        //     custom error - user already exists
-        //     next();
-        // }
-
         const generatedOtp = generateRandomOtp();
-
-        console.log(generatedOtp);
-
         let key = redisUtils["OTP_DATA"] + mobileNumber;
-
         let keyExpiryTime = redisUtils["OTP_EXPIRE_TIME"];
-
         await setCache(key, generatedOtp, keyExpiryTime);
-
-        // const redisClient = await getRedisWriterClient();
-
-        // await redisClient.set(mobileNumber, generatedOtp, {
-        //     EX: 120,
-        // });
-
         return res.status(HTTP_STATUS_CODE.OK).send(successRes({}, API_RESPONSE_MESSAGE.SUCCESS));
     } catch (error) {
         next(error);
     }
 };
+
+export const verifyOtp = async (req: any, res: Response, next: NextFunction) => {
+    try {
+        const { mobileNumber, otp } = req.body;
+
+        let key = redisUtils["OTP_DATA"] + mobileNumber;
+        const cachedOtp = await getCache(key);
+        if(cachedOtp !== otp) {
+            throw new Api401Error(API_RESPONSE_MESSAGE.UNAUTHORIZED);
+        }
+        // jwt logic here
+        return res.status(HTTP_STATUS_CODE.OK).send(successRes({}, API_RESPONSE_MESSAGE.SUCCESS));
+    } catch (error) {
+        next(error);
+    }
+}
